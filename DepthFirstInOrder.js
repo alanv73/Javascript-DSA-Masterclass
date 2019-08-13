d9716a5e03543bb282af8df66ba3b7db2583d7c1
Alan Van Art <alanv73@gmail.com>
2019-08-13 11:34:14 -0400

    tree traversal - BFS/DFS

---
+++ DepthFirstInOrder.js
@@ -0,0 +1,137 @@
+// Depth First Search - In Order
+// used with BST's returns items in ascending order
+
+class Node {
+    constructor(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(val){
+        let newNode = new Node(val);
+        if(!this.root) {
+            this.root = newNode;
+            return this;
+        }
+        let node = this.root;
+        while(true){
+            if(val === node.val) return undefined;
+            if(val < node.val){
+                //left
+                if(!node.left){
+                    node.left = newNode;
+                    return this;
+                }
+                node = node.left;
+            } else if(val > node.val) {
+                // right
+                if(!node.right){
+                    node.right = newNode;
+                    return this;
+                }
+                node = node.right;
+            }
+        }
+    }
+    find(val) {
+        if(!this.root) return false;
+        let found = false;
+        let node = this.root;
+        while(!found && node){
+            if(val < node.val){
+                node = node.left;
+            } else if(val > node.val) {
+                node = node.right;
+            } else {
+                found = true;
+            }
+        }
+        return node || undefined;
+    }
+    contains(val) {
+        if(!this.root) return false;
+        let node = this.root;
+        while(node){
+            if(val < node.val){
+                node = node.left;
+            } else if(val > node.val) {
+                node = node.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    bfs(){
+        if(!this.root) return [];
+        let q = [];
+        let values = [];
+        let current = this.root;
+        q.push(current);
+        while(q.length){
+            current = q.shift();
+            values.push(current.val);
+            if(current.left) q.push(current.left);
+            if(current.right) q.push(current.right);
+        }
+        return values;
+    }
+    dfsPreOrder(){
+        if(!this.root) return [];
+        let values = [];
+        let current = this.root;
+        function traverse(node) {
+            values.push(node.val);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(current);
+        return values;
+    }
+    dfsPostOrder(){
+        if(!this.root) return [];
+        let values = [];
+        let current = this.root;
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            values.push(node.val);
+        }
+        traverse(current);
+        return values;
+    }
+    dfsInOrder(){
+        if(!this.root) return [];
+        let values = [];
+        let current = this.root;
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            values.push(node.val);
+            if(node.right) traverse(node.right);
+        }
+        traverse(current);
+        return values;
+    }
+    
+}
+
+
+var tree = new BinarySearchTree();
+//                     10
+//               6           15
+//           3       8            20
+
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+tree.dfsInOrder(); //[3,6,8,10,15,20]
